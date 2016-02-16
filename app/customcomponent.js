4e69dc58c2c2969de3958f553093327a2a904696
Enrique Alcantara <enriquealc27@gmail.com>
2016-02-16 16:14:18 -0400

    version of the example project to test the existence of a module

--- app/customcomponent.js
+++ app/customcomponent.js
@@ -3,27 +3,8 @@
      * Create an Angular module that serves as the entry point
      * to all the other modules needed to make your component work
      */
-    var displayDatasetMod =  angular.module('displayDataset', []),
-        myCustomComponent = angular.module('myCustomComponent', [displayDatasetMod.name])
+    var testModule =  angular.module('testModule', []),
+        myCustomComponent = angular.module('myCustomComponent', [testModule.name])
 
-
-    function DisplayDatasetInfoDirective(currentDataset) {
-
-        return {
-            restrict : 'E',
-            template : '<div>{{dataset.name}}</div>',
-            link : function(scope) {
-                currentDataset.fetch().then(function(dataset) {
-                    scope.dataset = dataset
-                })
-            }
-        }
-    }
-
-    DisplayDatasetInfoDirective.$inject = [
-        'currentDataset'
-    ]
-
-    //You can use all the services and directives already defined by whaam
-    displayDatasetMod.directive('displayDatasetInfo', DisplayDatasetInfoDirective)
+    testModule.value('testValue', { })
 })()
